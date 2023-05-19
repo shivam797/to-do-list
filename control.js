634190d7ba3107011236de4054cd27727a3e6802
shivam797 <shivamkumar004667@gmail.com>
2023-05-19 10:26:52 +0530

    first project to do list

--- control.js
+++ control.js
@@ -2,8 +2,10 @@ console.log('hello');
 
 let list = document.getElementById('list-container');
 let arr = [];
+let count = 0;
 // let element = document.createElement('li');
 // let span = document.createElement('span');
+
 
 function myfunction(){
     let text = document.getElementById('input'); 
@@ -28,7 +30,9 @@ function myfunction(){
         // }
         element.innerHTML = '<input type = "checkbox" >' + text.value;
         list.appendChild(element);
-        element.appendChild(span)
+        element.appendChild(span);
+        count = count+1;
+        document.getElementById('count').innerHTML = count;
         text.value = "";
         saveItem();
 
@@ -36,18 +40,22 @@ function myfunction(){
 }
 
     list.addEventListener("click", function(e){
-        console.log(e.target);
+        console.log(e.target.tagName);
+        if (e.target.tagName == 'INPUT'){
         let checkbox = e.target;
         let parent = checkbox.parentNode;
         parent.classList.toggle('toggle');
-        // console.log(e.target.tagName);
-        if (e.target.tagName == 'LI'){
-            let li = e.target;
-            console.log(li.checked);
         }
+        // console.log(e.target.tagName);
+        // if (e.target.tagName == 'LI'){
+        //     let li = e.target;
+        //     console.log(li.checked);
+        // }
         if (e.target.tagName === 'SPAN'){
             // console.log(e.target.tagName);
             e.target.parentElement.remove();
+            count-=1;
+            document.getElementById('count').innerHTML = count;
             saveItem();
         }
     });
